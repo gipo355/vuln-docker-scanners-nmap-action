02c30da757aef65082eb2b9684fc5ab71d80e6b4
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-06-10 12:37:41 +0200

    test sarif creation

--- src/parse-nmap-json-to-sarif.spec.ts
+++ src/parse-nmap-json-to-sarif.spec.ts
@@ -1,11 +1,14 @@
 import { expect, test, describe } from "vitest";
+import fs from "fs";
 
 import {
   convertNmapJsonToSarif,
   readNmapJson,
+  writeSarifReport,
 } from "./parse-nmap-json-to-sarif";
 
 let json: object | null = null;
+let report: object | null = null;
 
 describe("parseNmapJsonToSarif", () => {
   test("should load json file into memory", () => {
@@ -17,10 +20,25 @@ describe("parseNmapJsonToSarif", () => {
   });
 
   test("should convert nmap json to sarif", () => {
-    const report = convertNmapJsonToSarif(json);
+    report = convertNmapJsonToSarif(json);
 
     console.log(JSON.stringify(report, null, 2));
 
     expect(report).toBeDefined();
   });
+
+  test("should create a sarif file", () => {
+    const path = "test/assets/vulners-report.sarif";
+    const modifiedPath = writeSarifReport(path, report);
+
+    console.log(modifiedPath);
+
+    const file = fs.readFileSync(modifiedPath, "utf8");
+
+    // expect file to be string
+    expect(typeof file).toBe("string");
+  });
+
+  // validate sarif with
+  // https://sarifweb.azurewebsites.net/Validation
 });
